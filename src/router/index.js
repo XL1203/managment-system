eb3e81d79bc129b5cb62c3f88979b89a3ead40b8
XL1203 <473616916@qq.com>
2020-10-13 13:50:51 +0800

    数据列表完成

--- src/router/index.js
+++ src/router/index.js
@@ -11,7 +11,7 @@ import Params from '../components/goods/Params'
 import List from '../components/goods/List'
 import AddGoods from '../components/goods/Add'
 import Order from '../components/order/Order'
-
+import Report from '../components/report/Report'
 
 Vue.use(VueRouter)
 
@@ -29,7 +29,8 @@ const routes = [{ path: '/', redirect: '/login' },
             { path: '/params', component: Params },
             { path: '/goods', component: List },
             { path: '/goods/add', component: AddGoods },
-            { path: '/orders', component: Order }
+            { path: '/orders', component: Order },
+            { path: '/reports', component: Report }
         ]
     }
 ]
